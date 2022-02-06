4de60dc17f98cd3838e231f76105a1ed7b53201a
Maulik Savaliya <savaliyamaulik98@gmail.com>
2022-02-06 14:22:25 +0530

    error handling

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,6 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 // Create Product -- Admin
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.create(req.body);
-
   res.status(201).json({ success: true, product });
 });
 
--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,29 @@ const dotenv = require("dotenv");
 
 const connectDatabase = require("./config/database");
 
+// Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
+
 // config
 dotenv.config({ path: "backend/config/config.env" });
 
 // connect to database
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`server is working on http://localhost:${process.env.PORT}`);
+});
+
+// Unhandled Promises Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandle Promises Rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
